60a9738694cdc7eee503246f3bb5e4e0bd640431
Judah Schwartz <juschwartz@paloaltonetworks.com>
2022-01-06 09:40:04 +0200

    Null safe calls

--- Packs/CrowdStrikeFalconSandboxV2/Integrations/CrowdstrikeFalconSandboxV2/CrowdstrikeFalconSandboxV2.py
+++ Packs/CrowdStrikeFalconSandboxV2/Integrations/CrowdstrikeFalconSandboxV2/CrowdstrikeFalconSandboxV2.py
@@ -24,7 +24,7 @@ from typing import Dict, Any
 requests.packages.urllib3.disable_warnings()  # pylint: disable=no-member
 
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'  # ISO8601 format with UTC, default in XSOAR
-SEARCH_TERM_QUERY_ARGS = ('file_name', 'file_type', 'file_type_desc', 'env_id', 'country', 'verdict', 'av_detect',
+SEARCH_TERM_QUERY_ARGS = ('filename', 'filetype', 'filetype_desc', 'env_id', 'country', 'verdict', 'av_detect',
                           'vx_family', 'tag', 'date_from', 'date_to', 'port', 'host', 'domain', 'url', 'similiar_to',
                           'context', 'imp_hash', 'ssdeep', 'authentihash')
 
@@ -38,7 +38,23 @@ class Client(BaseClient):
         return self._http_request(method='GET', url_suffix=f"/report/{key}/screenshots")
 
     def search(self, query_args: Dict[str, Any]):
-        pass
+        self._headers['Content-Type'] = "application/x-www-form-urlencoded"
+        return self._http_request(method='POST', url_suffix=f'/search/terms', data=query_args)
+
+    def scan(self, files: List[str]):
+        self._headers['Content-Type'] = "application/x-www-form-urlencoded"
+        return self._http_request(method='POST', url_suffix='/search/hashes', data={'hashes[]': files})
+
+
+def map_object(obj: Any, maprules: Dict[str, str], only_given_fields=False):
+    # class InlineClass(object):
+    #     def __init__(self, dict):
+    #         self.__dict__ = dict
+    #
+    # obj = InlineClass({'propertyName': 'propertyValue'})
+    return type('obj', (object,),
+                {maprules.get(key, key): obj.__dict__[key]
+                 for key in obj.__dict__.keys() if not only_given_fields or key in maprules})
 
 
 def translate_verdict(param: str):
@@ -54,38 +70,44 @@ def translate_verdict(param: str):
 
 
 def split_query_to_term_args(query: str) -> Dict[str, Any]:
-    pass
+    def get_value(term):
+        return term[term.index(':') + 1:]
+
+    def get_key(term):
+        return term[:term.index(':')]
+
+    return {get_key(term): get_value(term) for term in query.split(',') if get_value(term)}
 
 
-def validated_term(pair):
-    if pair[0] == 'verdict':
-        return translate_verdict(pair[1])
-    if pair[0] == 'country' and len(pair[1]) != 3:
+def validated_term(key, val):
+    if key == 'verdict':
+        return translate_verdict(val)
+    if key == 'country' and len(val) != 3:
         raise ValueError('Country ISO code should be 3 characters long')
-    return pair[1]
+    return val
 
 
 def validated_search_terms(query_args: Dict[str, Any]) -> Dict[str, Any]:
     if len(query_args) == 0:
         raise ValueError('Must have at least one search term')
-    return {pair[0]: validated_term(pair) for pair in query_args}
+    return {key: validated_term(key, query_args[key]) for key in query_args}
 
 
 def get_search_term_args(args) -> Dict[str, Any]:
-    if args['query']:
+    if args.get('query'):
         return split_query_to_term_args(args['query'])
     else:
-        return {term: args[term] for term in SEARCH_TERM_QUERY_ARGS if args[term]}
+        return {term: args[term] for term in SEARCH_TERM_QUERY_ARGS if args.get(term, None)}
 
 
 def to_image_result(image):
     return fileResult(image['name'], base64.b64decode(image['image']), entryTypes['entryInfoFile'])
 
 
-def get_key(args):
-    if args['file'] and args['environmentID']:
+def get_api_id(args):
+    if args.get('file') and args.get('environmentID'):
         return f"{args['file']}:{args['environmentID']}"
-    elif args['jobID']:
+    elif args.get('jobID'):
         return args['jobID']
     else:
         raise ValueError('Must supply job_id or environment_id and file')
@@ -126,14 +148,14 @@ def crowdstrike_get_environments_command(client: Client):
         outputs_prefix='CrowdStrike.Environment',
         outputs_key_field='id',
         outputs=environments,
-        readable_output=tableToMarkdown('All Environments:', environments, ['id', 'description'],
+        readable_output=tableToMarkdown('All Environments:', environments, ['id', 'description'], removeNull=True,
                                         headerTransform=lambda x: {'id': '_ID', 'description': 'Description'}[x]),
         raw_response=environments
     )
 
 
 def crowdstrike_get_screenshots_command(client: Client, args: Dict[str, Any]):
-    key = get_key(args)
+    key = get_api_id(args)
     return [to_image_result(image) for image in client.get_screenshots(key)]
 
 
@@ -144,8 +166,22 @@ def crowdstrike_result_command():
 def crowdstrike_search_command(client: Client, args):
     query_args: Dict = get_search_term_args(args)
     query_args = validated_search_terms(query_args)
+    response = client.search(query_args)
 
-    return client.search(query_args)
+    def convert_to_file_res(res):
+        return Common.File(size=res['size'], sha256=res['sha256'], dbot_score=None, extension=res['type_short'],
+                           name=res['submit_name'])
+
+    return CommandResults(
+        raw_response=response,
+        outputs_prefix='CrowdStrike.Search',
+        outputs=response['result'],
+        indicators=[convert_to_file_res(res) for res in response['result']]
+    )
+
+
+def crowdstrike_scan_command(client: Client, args):
+    client.scan(args['file'].split(','))
 
 
 def main() -> None:
@@ -183,15 +219,18 @@ def main() -> None:
             result = test_module(client)
             return_results(result)
 
+        elif demisto.command() in ('cs-falcon-sandbox-search', 'crowdstrike-search'):
+            return_results(crowdstrike_search_command(client, args))  # TODO ret
+        elif demisto.command() in ('cs-falcon-sandbox-scan', 'crowdstrike-scan', 'file'):
+            crowdstrike_scan_command(client, args)  # TODO ret:
+        elif demisto.command() == 'cs-falcon-sandbox-analysis':
+            pass
         elif demisto.command() in ('crowdstrike-get-environments', 'cs-falcon-sandbox-get-environments'):
             return_results(crowdstrike_get_environments_command(client))
         elif demisto.command() in ('cs-falcon-sandbox-get-screenshots', 'crowdstrike-get-screenshots'):
             demisto.results(crowdstrike_get_screenshots_command(client, args))
         elif demisto.command() in ('cs-falcon-sandbox-result', 'crowdstrike-result'):
-            crowdstrike_result_command()
-        elif demisto.command() in ('cs-falcon-sandbox-search', 'crowdstrike-search'):
-            crowdstrike_search_command(client, args)
-
+            return_results(crowdstrike_result_command())
     # Log exceptions and return errors
     except Exception as e:
         demisto.error(traceback.format_exc())  # print the traceback
@@ -200,5 +239,5 @@ def main() -> None:
 
 ''' ENTRY POINT '''
 
-# if __name__ in ('__main__', '__builtin__', 'builtins'): TODO return line
+# if __name__ in ('__main__', '__builtin__', 'builtins'): #TODO put me back
 main()
---
+++ Packs/CrowdStrikeFalconSandboxV2/Integrations/CrowdstrikeFalconSandboxV2/CrowdstrikeFalconSandboxV2_test.py
@@ -0,0 +1,70 @@
+"""Base Integration for Cortex XSOAR - Unit Tests file
+
+Pytest Unit Tests: all funcion names must start with "test_"
+
+More details: https://xsoar.pan.dev/docs/integrations/unit-testing
+
+MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
+
+You must add at least a Unit Test function for every XSOAR command
+you are implementing with your integration
+"""
+
+import json
+import io
+
+from Packs.CrowdStrikeFalconSandboxV2.Integrations.CrowdstrikeFalconSandboxV2.CrowdstrikeFalconSandboxV2 import \
+    validated_search_terms, split_query_to_term_args, map_object
+
+
+def util_load_json(path):
+    with io.open(path, mode='r', encoding='utf-8') as f:
+        return json.loads(f.read())
+
+
+# # TODO: REMOVE the following dummy unit test function
+# def test_baseintegration_dummy():
+#     """Tests helloworld-say-hello command function.
+#
+#     Checks the output of the command function with the expected output.
+#
+#     No mock is needed here because the say_hello_command does not call
+#     any external API.
+#     """
+#     from BaseIntegration import Client, baseintegration_dummy_command
+#
+#     client = Client(base_url='some_mock_url', verify=False)
+#     args = {
+#         'dummy': 'this is a dummy response'
+#     }
+#     response = baseintegration_dummy_command(client, args)
+#
+#     mock_response = util_load_json('test_data/baseintegration-dummy.json')
+#
+#     assert response.outputs == mock_response
+# # TODO: ADD HERE unit tests for every command
+def test_validated_search_terms():
+    pre_validation = {"hello": "world", "verdict": 'NoSpecificThreat'}
+    post_validation = validated_search_terms(pre_validation)
+    assert post_validation == {'hello': 'world', 'verdict': 3}
+
+
+def test_split_query_to_term_args():
+    query = 'hello:world,three:split:fine,heelo:'
+    res = split_query_to_term_args(query)
+    assert {'hello': 'world', 'three': 'split:fine'} == res
+
+def test_map_object():
+    class InlineClass(object):
+        def __init__(self, dict):
+            self.__dict__ = dict
+
+    obj = InlineClass({'propertyName': 'propertyValue', 'a' : 'b', 'x' : 'y'})
+
+    res = map_object(obj, {'a' : 'c', 'x' : 'z'},True)
+    assert res.c == 'b'
+    assert res.__dict__.get('.propertyName', None) is None
+
+    res = map_object(obj, {'a' : 'c', 'x': 'z'}, False)
+    assert res.z == 'y'
+    assert res.propertyName == 'propertyValue'
